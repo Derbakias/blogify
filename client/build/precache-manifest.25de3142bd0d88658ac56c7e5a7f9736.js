d75e558f1d317d528bfc9685e8ab4f9df6afc6bd
derbakias <dimitrios.danousis@gmail.com>
2020-09-07 18:36:20 +0100

    add build

---
+++ client/build/precache-manifest.25de3142bd0d88658ac56c7e5a7f9736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aaced5a35d0d728b6ee7cbe75897d51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0877dfa716b5bf75ba57",
+    "url": "./static/css/main.6a16c6fd.chunk.css"
+  },
+  {
+    "revision": "baa093d6fcd72ab211bc",
+    "url": "./static/js/2.1cafcfa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1cafcfa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0877dfa716b5bf75ba57",
+    "url": "./static/js/main.d75e1c43.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
